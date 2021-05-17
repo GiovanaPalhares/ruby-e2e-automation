75efcff9bc1478a9c59067d94d95dbd0b1e1dd38
GiovanaPalhares <gopalhares@hotmail.com>
2021-05-17 20:54:42 -0300

    wip

--- features/step_definitions/cadastro_steps.rb
+++ features/step_definitions/cadastro_steps.rb
@@ -9,7 +9,7 @@
     find("#password").set "abc123"
 
     click_button "Cadastrar"
-    sleep 10
+   
   end
   
   Então('sou redirecionado para o Dashboard') do
@@ -26,3 +26,36 @@
     alert = find(".alert-dark")
     expect(alert.text).to eql "Oops. Informe seu nome completo!"
   end
+
+  Quando('submeto o meu cadastro sem o email') do
+    find("#fullName").set "Giovana"
+    find("#password").set "abc123"
+
+    click_button "Cadastrar"
+  end
+  
+  Então('vejo a mensagem de alerta: Oops. Informe um email válido!') do
+    alert = find(".alert-dark")
+    expect(alert.text).to eql "Oops. Informe um email válido!"
+  end
+
+  Quando('submeto o meu cadastro com email incorreto') do
+    find("#fullName").set "Giovana"
+    find("#email").set "giovana#hotmail.com"
+    find("#password").set "abc123"
+
+    click_button "Cadastrar"
+  end
+
+  Quando('submeto o meu cadastro sem a senha') do
+    find("#fullName").set "Giovana"
+    find("#email").set Faker::Internet.free_email
+  
+    click_button "Cadastrar"
+  end
+  
+  Então('vejo a mensagem de alerta: Oops. Informe sua senha secreta!') do
+    alert = find(".alert-dark")
+    expect(alert.text).to eql "Oops. Informe sua senha secreta!"
+  end
+  
