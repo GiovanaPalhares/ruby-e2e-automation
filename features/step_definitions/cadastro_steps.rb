c5efbd2cc07b1d023e7331b10ccd153c83b6aa63
GiovanaPalhares <gopalhares@hotmail.com>
2021-06-10 21:34:58 -0300

    cucumber arguments implementation

--- features/step_definitions/cadastro_steps.rb
+++ features/step_definitions/cadastro_steps.rb
@@ -26,10 +26,6 @@
     click_button "Cadastrar"
   end
   
-  Então('vejo a mensagem de alerta: Oops. Informe seu nome completo!') do
-    alert = find(".alert-dark")
-    expect(alert.text).to eql "Oops. Informe seu nome completo!"
-  end
 
   Quando('submeto o meu cadastro sem o email') do
     find("#fullName").set "Giovana"
@@ -38,10 +34,6 @@
     click_button "Cadastrar"
   end
   
-  Então('vejo a mensagem de alerta: Oops. Informe um email válido!') do
-    alert = find(".alert-dark")
-    expect(alert.text).to eql "Oops. Informe um email válido!"
-  end
 
   Quando('submeto o meu cadastro com email incorreto') do
     find("#fullName").set "Giovana"
@@ -58,8 +50,9 @@
     click_button "Cadastrar"
   end
   
-  Então('vejo a mensagem de alerta: Oops. Informe sua senha secreta!') do
+
+  Então('vejo a mensagem de alerta: {string}') do |expect_alert|
     alert = find(".alert-dark")
-    expect(alert.text).to eql "Oops. Informe sua senha secreta!"
+    expect(alert.text).to eql expect_alert
   end
   
