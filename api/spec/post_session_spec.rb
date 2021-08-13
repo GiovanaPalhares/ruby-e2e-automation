5a80bd0788f62e662390c11838c316a0cd386549
GiovanaPalhares <gopalhares@hotmail.com>
2021-08-12 21:48:01 -0300

    resolving method problem

--- api/spec/post_session_spec.rb
+++ api/spec/post_session_spec.rb
@@ -1,16 +1,9 @@
-require 'httparty'
+require_relative 'routes/sessions'
 
 describe "POST/sessions" do 
     context "login com sucesso" do 
         before(:all) do
-            payload = { email: "giovana@hotmail.com", password: "123456"}
-            @result = HTTParty.post(
-            'http://rocklov-api:3333/sessions',
-            body: payload.to_json,
-            headers: {
-                "Content-Type": "application/json"
-            }
-        ) 
+            @result = Sessions.new.login("giovana@hotmail.com", "123456")
         end
 
         it "status code 200" do
@@ -19,6 +12,20 @@ describe "POST/sessions" do
 
         it "id com 24 caracteres" do
             expect(@result.parsed_response["_id"].length).to eql 24
+        end
+    end
+
+    context "login com senha invalida" do 
+        before(:all) do
+            @result = Sessions.new.login("giovana@hotmail.com", "648961651651561")
+        end
+
+        it "status code 401" do
+            expect(@result.code).to eql 401
+        end
+
+        it "id com 24 caracteres" do
+            expect(@result.parsed_response["error"]).to eql "Unauthorized"
         end
     end
 end
