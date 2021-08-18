c3c5dccc8b83bd8d83827c16b2ebd0c307b4fba9
GiovanaPalhares <gopalhares@hotmail.com>
2021-08-18 19:37:15 -0300

    Removing user from DB

--- api/spec/signup_sessions_spec.rb
+++ api/spec/signup_sessions_spec.rb
@@ -1,9 +1,11 @@
 require_relative 'routes/signup_sessions'
+require_relative 'libs/mongo'
 
 describe "signup sessions" do
     context "cadastro de usuário com sucesso" do 
         before(:all) do
             payload = {name: "joao", email: "joao@hotmail.com", password: "123456"}
+            Mongodb.new.remove_user(payload[:email])
             @result = Signup_sessions.new.sign_up(payload)
         end
     
