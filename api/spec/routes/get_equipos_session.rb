0286dfabb46e1c38fa637f515d6a099ce491c9bb
GiovanaPalhares <gopalhares@hotmail.com>
2021-09-04 17:57:07 -0300

    get equipos refactor

--- api/spec/routes/get_equipos_session.rb
+++ api/spec/routes/get_equipos_session.rb
@@ -2,10 +2,9 @@ require_relative "base_api"
 
 class Get_equipo < Base_api
 
-    def get_equipos(equipo_name, user_id)
+    def get_equipos(user_id)
         return self.class.get(
             '/equipos',
-            query: equipo_name,
             headers: {
                 "user_id": user_id
             }
--- api/spec/scenarios/get_equipos_spec.rb
+++ api/spec/scenarios/get_equipos_spec.rb
@@ -1,27 +1,49 @@
 describe "get_equipo" do 
 
     before(:all) do 
-        login_payload = { email: "giovana@hotmail.com", password: "123456" }
+        login_payload = { email: "giovana10@hotmail.com", password: "123456" }
         login = Sessions.new.login(login_payload)
         @user_id = login.parsed_response["_id"]  
     end
 
-    def get_name()
-        thumbnail = Helpers::get_thumb("slash.jpg") #implementado no helpers a busca pelo arquivo de imagem
-        payload = { thumbnail: thumbnail, name: "Microfone", category: "Cordas", price: 100 }
-        Mongodb.new.remove_equipo(payload[:name], @user_id)
-        Post_equipo.new.post_equipo(payload, @user_id)
-        return payload[:name]
-    end
-
     context "obter equipos de usuário" do
         before(:all) do 
-            @get_equipo = Get_equipo.new.get_equipos(get_name(), @user_id)
+            payloads = [
+                { 
+                    thumbnail: Helpers::get_thumb("sanfona.jpg"),
+                    name: "Guitarra",
+                    category: "Cordas",
+                    price: 100 
+                },
+                { 
+                    thumbnail: Helpers::get_thumb("trompete.jpg"),
+                    name: "Guitarra",
+                    category: "Cordas",
+                    price: 100 
+                },
+                { 
+                    thumbnail: Helpers::get_thumb("slash.jpg"),
+                    name: "Guitarra",
+                    category: "Cordas",
+                    price: 100 
+                }
+            ]
+
+            payloads.each do |payload|
+                Mongodb.new.remove_equipo(payload[:name], @user_id)
+                Post_equipo.new.post_equipo(payload, @user_id)
+            end
+
+            @get_equipo = Get_equipo.new.get_equipos(@user_id)
         end
 
         it "deve retornar 200" do 
             expect(@get_equipo.code).to eql 200
         end 
+
+        it "deve retornar uma lista de equipos" do  
+            expect(@get_equipo.parsed_response).not_to be_empty    
+        end
     end
 
 end
