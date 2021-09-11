7f6ccbfc6801ec9bed47a545a06a499acbc2033a
GiovanaPalhares <gopalhares@hotmail.com>
2021-09-11 11:43:12 -0300

    signing up users before test

--- api/spec/spec_helper.rb
+++ api/spec/spec_helper.rb
@@ -9,6 +9,11 @@ require_relative 'routes/get_equipos_session.rb'
 require_relative 'routes/delete_equipo'
 require_relative 'routes/booking_equipo'
 
+require "digest/md5"
+
+def to_md5(pass)
+  return Digest::MD5.hexdigest(pass)
+end
 
 RSpec.configure do |config|
   
@@ -16,15 +21,25 @@ RSpec.configure do |config|
     
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
-
   
   config.mock_with :rspec do |mocks|
     
     mocks.verify_partial_doubles = true
   end
 
-  
   config.shared_context_metadata_behavior = :apply_to_host_groups
+
+  config.before(:suite) do 
+    users = [
+      { name: "joao", email: "joao@hotmail.com", password: to_md5("123456") },
+      { name: "maria1", email: "maria1@hotmail.com", password: to_md5("123456") },
+      { name: "Giovana", email: "giovana@hotmail.com", password: to_md5("123456") },
+      { name: "Giovana Silva", email: "giovana10@hotmail.com", password: to_md5("123456") }
+    ]
+
+    Mongodb.new.drop_table
+    Mongodb.new.insert_users(users)
+  end
 
 
 end
