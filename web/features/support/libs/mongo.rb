0c0c50cbaa1788e959a6e07b0727525296dc6bd3
GiovanaPalhares <gopalhares@hotmail.com>
2021-11-18 18:14:04 -0300

    Insert user automatically using Rakefile

--- web/features/support/libs/mongo.rb
+++ web/features/support/libs/mongo.rb
@@ -1,28 +1,41 @@
 require 'mongo'
+require 'pry'
 
 Mongo::Logger.logger = Logger.new("./logs/mongo.log")
 
 class Mongodb
+    attr_accessor :client, :users, :equipos
 
     def remove_user(email)
-        client = Mongo::Client.new('mongodb://rocklov-db:27017/rocklov')
+        client = Mongo::Client.new(CONFIG["mongo"])
         users = client[:users]
         users.delete_many({email: email})
     end
 
     def get_user(email)
-        client = Mongo::Client.new('mongodb://rocklov-db:27017/rocklov')
+        client = Mongo::Client.new(CONFIG["mongo"])
         users = client[:users]
         user = users.find({email: email}).first
         return user[:_id]
     end
-
 
     def remove_equipo(name, email)
         user_id = get_user(email)
         client = Mongo::Client.new(CONFIG["mongo"])
         equipos = client[:equipos]
         equipos.delete_many({name: name, user: user_id})
+    end
+
+    def drop_table
+        #binding.pry
+        client = Mongo::Client.new(CONFIG["mongo"])
+        client.database.drop
+    end
+    
+    def insert_users(docs)
+        client = Mongo::Client.new(CONFIG["mongo"])
+        users = client[:users]
+        users.insert_many(docs)
     end
 
 end
